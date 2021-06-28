7ac1514a39f09b387aadc4dc561a987e5bbc0113
maxsoe-dscale <max.soe@dscale.io>
2021-06-28 16:22:57 +0400

    Step 6

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -32,21 +32,32 @@ const List<String> cryptoList = [
 ];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const apiKey = '1C613784-530D-4F42-B413-05DE3C07C569';
 
 class CoinData {
   Future getCoinData(String selectedCurrency) async {
-    //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
-    //TODO 5: Return a Map of the results instead of a single value.
-    String requestURL = '$coinAPIURL/BTC$selectedCurrency';
-    http.Response response = await http.get(requestURL);
-    if (response.statusCode == 200) {
-      var decodedData = jsonDecode(response.body);
-      var lastPrice = decodedData['rate'];
-      return lastPrice;
-    } else {
-      print(response.statusCode);
-      throw 'Problem with the get request';
+//4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    Map<String, double> pairs ={};
+
+    for (String coin in cryptoList) {
+      // print(coin);
+      // print(selectedCurrency);
+      String requestURL = '$coinAPIURL/$coin/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(requestURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        // print(lastPrice);
+        pairs[coin] = lastPrice;
+
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
     }
+
+    //5: Return a Map of the results instead of a single value.
+    print('$selectedCurrency $pairs');
+    return pairs;
   }
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
 import 'dart:io' show Platform;
+import 'crypto_card.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -52,16 +53,16 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  String value = '?';
+  Map value = {};
 
   //TODO 7: Figure out a way of displaying a '?' on screen while we're waiting for the price data to come back. Hint: You'll need a ternary operator.
 
   //TODO 6: Update this method to receive a Map containing the crypto:price key value pairs. Then use that map to update the CryptoCards.
   void getData() async {
     try {
-      double data = await CoinData().getCoinData(selectedCurrency);
+      Map data = await CoinData().getCoinData(selectedCurrency);
       setState(() {
-        value = data.toStringAsFixed(0);
+        value = data;
       });
     } catch (e) {
       print(e);
@@ -86,30 +87,15 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          //TODO 1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
+          //1: Refactor this Padding Widget into a separate Stateless Widget called CryptoCard, so we can create 3 of them, one for each cryptocurrency.
           //TODO 2: You'll need to able to pass the selectedCurrency, value and cryptoCurrency to the constructor of this CryptoCard Widget.
           //TODO 3: You'll need to use a Column Widget to contain the three CryptoCards.
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $value $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+
+          Column(children: [
+            CryptoCard(selectedCurrency: selectedCurrency, value: value['BTC'], cryptoCurrency: 'BTC'),
+            CryptoCard(selectedCurrency: selectedCurrency, value: value['ETH'], cryptoCurrency: 'ETH'),
+            CryptoCard(selectedCurrency: selectedCurrency, value: value['LTC'], cryptoCurrency: 'LTC'),
+          ],),
           Container(
             height: 150.0,
             alignment: Alignment.center,
